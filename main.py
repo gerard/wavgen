e732a8056e0d5eeb2761895eee447129aed818f9
Gerard Lledo <gerard.lledo@gmail.com>
2010-02-14 18:31:20 +0200

    Fix: Samples are signed

--- main.py
+++ main.py
@@ -55,6 +55,6 @@ sys.stdout.write(B_WAV1_Chunk)
 sys.stdout.write(B_WAV2_Header)
 
 for i in range(n_samples):
-    sample = ((2**15) - 1) * math.sin((i%40) * (2 * math.pi/40)) + (2**15)
-    sys.stdout.write(struct.pack('H', sample))
+    sample = ((2**15) - 1) * math.cos((i%20) * (2 * math.pi/20))
+    sys.stdout.write(struct.pack('h', sample))
 
